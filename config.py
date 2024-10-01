04130285c413421a57eac1865aabe363a927e530
Yulia Vdovina <yulya_vdovina_2000@mail.ru>
2024-10-01 22:26:31 +0300

    правки

--- config.py
+++ config.py
@@ -23,8 +23,6 @@ def to_driver_options(context):
         options.set_capability('platformVersion', os.getenv('PLATFORM_VERSION'))
         options.set_capability('appWaitActivity', os.getenv('APP_WAIT_ACTIVITY'))
         options.set_capability('app', os.getenv('APP'))
-        # load_dotenv(dotenv_path=file.path_from_project(
-        #     '..env.bstack'))
         options.set_capability(
             'bstack:options', {
                 'projectName': 'Wikipedia project',

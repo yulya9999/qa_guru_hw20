08b5c5cefe7b3039d20d40e71d28433a943e2b43
Yulia Vdovina <yulya_vdovina_2000@mail.ru>
2024-10-01 22:02:10 +0300

    правки

--- wikipedia_mobile_project_tests/pages/welcome_page.py
+++ wikipedia_mobile_project_tests/pages/welcome_page.py
@@ -45,26 +45,5 @@ class WelcomePage:
         self.click_continue_on_four_page()
         return self
 
-    # def welcome_screen(self):
-    #     with allure.step('Нажатие кнопки "Continue" на первой странице'):
-    #         browser.all((AppiumBy.ID, f'{resource_id}/option_label')).first.should(have.text('English'))
-    #         browser.element((AppiumBy.ID, f"{resource_id}/fragment_onboarding_forward_button")).click()
-    #
-    #     with allure.step('Нажатие кнопки "Continue" на второй странице'):
-    #         browser.element((AppiumBy.ID, f'{resource_id}/primaryTextView')).should(
-    #             have.text('New ways to explore'))
-    #         browser.element((AppiumBy.ID, f"{resource_id}/fragment_onboarding_forward_button")).click()
-    #
-    #     with allure.step('Нажатие кнопки "Continue" на третьей странице'):
-    #         browser.element((AppiumBy.ID, f"{resource_id}/primaryTextView")).should(
-    #             have.text('Reading lists with sync'))
-    #         browser.element((AppiumBy.ID, f"{resource_id}/fragment_onboarding_forward_button")).click()
-    #
-    #     with allure.step('Нажатие кнопки "Get started" на четвертой странице'):
-    #         browser.element((AppiumBy.ID, f"{resource_id}/primaryTextView")).should(have.text('Data & Privacy'))
-    #         browser.element((AppiumBy.ID, f"{resource_id}/fragment_onboarding_done_button")).click()
-    #
-    #     return self
-
 
 welcome_screen_page = WelcomePage()
